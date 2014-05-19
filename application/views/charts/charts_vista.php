8a37d64c9c94d09a6d47b4cd6d185be1138ddbc6
Rick07 <ricardosamz@gmail.com>
2014-05-19 17:12:12 -0500

    añadiendo tablas jtable

--- application/views/charts/charts_vista.php
+++ application/views/charts/charts_vista.php
@@ -1,4 +1,4 @@
-        <button id="target">MOSTRAR BOTON</button>
+        <button id="target">MOSTRAR GRÁFICO</button>
         <div id="container" style="min-width: 400px; height: 400px; margin: 0 auto"></div>
         <script type="text/javascript" src="<?php echo base_url()?>js/plotenergy.js"></script>
         <script src="<?php echo base_url()?>js/highcharts.js"></script>
--- application/views/main_vista.php
+++ application/views/main_vista.php
@@ -4,11 +4,54 @@
     <title>ECOWEB</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <!-- Bootstrap -->
-    <script src="<?php echo base_url()?>js/jquery-1.8.2.min.js"></script>
     <link href="<?php echo base_url()?>bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <script src="<?php echo base_url()?>bootstrap/js/bootstrap.min.js"></script>
+    <link href="<?php echo base_url()?>css/ui-lightness/jquery-ui-1.10.4.css" rel="stylesheet">
+    <!-- Jquery -->
+    <script src="<?php echo base_url()?>js/jquery-1.8.2.min.js"></script>
+    <script src="<?php echo base_url()?>js/jquery-ui-1.10.4.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url()?>js/navtabs.js"></script>
+    <!-- Include one of jTable styles. -->
+<link href="<?php echo base_url()?>js/jtable/themes/metro/blue/jtable.min.css" rel="stylesheet" type="text/css" />
+ 
+<!-- Include jTable script file. -->
+<script src="<?php echo base_url()?>js/jtable/jquery.jtable.js" type="text/javascript"></script>
+     <!-- Bootstrap -->
+    <script src="<?php echo base_url()?>bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    $(document).ready(function () {
+        $('#PersonTableContainer').jtable({
+            title: 'Table of people',
+            actions: {
+                listAction: '/GettingStarted/PersonList',
+                createAction: '/GettingStarted/CreatePerson',
+                updateAction: '/GettingStarted/UpdatePerson',
+                deleteAction: '/GettingStarted/DeletePerson'
+            },
+            fields: {
+                PersonId: {
+                    key: true,
+                    list: false
+                },
+                Name: {
+                    title: 'Author Name',
+                    type: 'textarea',
+                    width: '40%'
+                },
+                Age: {
+                    title: 'Age',
+                    width: '20%'
+                },
+                RecordDate: {
+                    title: 'Record date',
+                    width: '30%',
+                    type: 'date',
+                    create: false,
+                    edit: false
+                }
+            }
+        });
+    });
+</script>
   </head>
   <body>
      <img src="<?php echo base_url();?>images/logotipo.png" alt="logotipo" height="85" width="254">
@@ -19,5 +62,6 @@
   <li id="tablero"><a href="#tablero" data-toggle="tab">Tablero de datos</a></li>
 </ul>
 <div id="seccion"></div>
+<div id="PersonTableContainer" style="width: 600px;"></div>
 </body>
 </html>
